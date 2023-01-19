88e148b5234d5c4d35cb9e520fc871be6bde2ca2
ilotterytea <ilotterytea@proton.me>
2023-01-20 00:29:12 +0600

    Shared constants.

---
+++ src/main/java/kz/ilotterytea/neurobaj/SharedConstants.java
@@ -0,0 +1,10 @@
+package kz.ilotterytea.neurobaj;
+
+/**
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class SharedConstants {
+    /** The path to the properties file. */
+    public static final String PROPERTIES_PATH = "config.properties";
+}
