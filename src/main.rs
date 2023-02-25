3f4f3ceab5ad7ac7187788db4371f6142f29da8e
ilotterytea <ilotterytea@proton.me>
2023-02-25 18:22:09 +0600

    init chainmanager as static

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+#![feature(proc_macro_hygiene, decl_macro, once_cell)]
+
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
 mod chains;
+
+static CHAINS: Lazy<Mutex<chains::ChainManager>> =
+    Lazy::new(|| Mutex::new(chains::ChainManager::new()));
 
 fn main() {
     println!("Hello, world!");
