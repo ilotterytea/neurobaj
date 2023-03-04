933304103ba43f2d000d9a694af8a5a63da6a7a0
ilotterytea <ilotterytea@proton.me>
2023-03-04 15:11:11 +0600

    updated to the latest function version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #[macro_use]
 extern crate rocket;
 
-use diesel::{Connection, SqliteConnection};
+use chains::scan_text;
+use diesel::{Connection, QueryDsl, RunQueryDsl, SqliteConnection};
 use once_cell::sync::Lazy;
 use std::{env, fs::File, path::Path, sync::Mutex, time::Instant};
 use tokio_js_set_interval::set_interval;
@@ -11,33 +12,15 @@ use twitch_irc::{
 };
 
 mod chains;
+mod models;
 mod routes;
-
-static CHAINS: Lazy<Mutex<chains::ChainManager>> =
-    Lazy::new(|| Mutex::new(chains::ChainManager::new()));
-
-static CHANNELS: Lazy<Vec<String>> = Lazy::new(|| {
-    let mut _vec: Vec<String> = Vec::new();
-
-    if !Path::new("./channels.json").exists() {
-        return _vec;
-    }
-
-    let file = File::open("./channels.json").unwrap();
-    let mut _v: Vec<String> =
-        serde_json::from_reader(file).expect("JSON file with channels is not well formatted!");
-
-    _vec.append(&mut _v);
-
-    _vec
-});
+mod schema;
 
 static ELAPSED_TIME: Lazy<Mutex<Instant>> = Lazy::new(|| Mutex::new(Instant::now()));
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    CHAINS.lock().unwrap().load("./chains.json");
 
     let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(ClientConfig::default());
@@ -47,35 +30,22 @@ async fn main() {
             println!("Received message: {:?}", message);
 
             match message {
-                twitch_irc::message::ServerMessage::Privmsg(msg) => {
-                    CHAINS.lock().unwrap().scan_text(
-                        &msg.message_text,
-                        Some(chains::ChainSignature {
-                            msg_id: Some(msg.message_id),
-                            author_id: Some(msg.sender.id),
-                            channel_id: Some(msg.channel_id),
-                        }),
-                    );
-                }
+                twitch_irc::message::ServerMessage::Privmsg(msg) => scan_text(
+                    &mut establish_connection(),
+                    msg.sender.id.as_str(),
+                    msg.channel_id.as_str(),
+                    msg.message_id.as_str(),
+                    msg.message_text.as_str(),
+                ),
                 _ => {}
             }
         }
     });
 
-    for name in &CHANNELS.to_vec() {
-        client.join(name.to_owned()).unwrap();
-    }
-
     rocket::ignite()
         .mount("/api/v1", routes![routes::gen_text, routes::status])
         .launch();
 
-    set_interval!(
-        || {
-            CHAINS.lock().unwrap().save("./chains.json");
-        },
-        90000
-    );
     join_handle.await.unwrap();
 }
 
